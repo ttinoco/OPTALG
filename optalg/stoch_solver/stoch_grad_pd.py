372062e9a26f77113b1387c168e436ec84497589
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2015-12-08 13:29:11 +0100

    added some running averages

--- optalg/stoch_solver/stoch_grad_pd.py
+++ optalg/stoch_solver/stoch_grad_pd.py
@@ -25,12 +25,16 @@ class PrimalDual_StochasticGradient(StochasticSolver):
             print '{0:^10s}'.format('time(s)'),
             print '{0:^10s}'.format('prop'),
             print '{0:^10s}'.format('lmax'),
+            print '{0:^12s}'.format('EF_run'),
+            print '{0:^12s}'.format('EGmax_run'),
             print '{0:^12s}'.format('EF'),
             print '{0:^12s}'.format('EGmax')
 
         # Init
         t0 = time.time()
         lam = np.zeros(prob.get_size_lam())
+        EF_run = 0.
+        EG_run = np.zeros(prob.get_size_lam())
         
         # Loop
         for k in range(maxiters):
@@ -39,10 +43,14 @@ class PrimalDual_StochasticGradient(StochasticSolver):
             w = prob.sample_w()
             
             # Eval
-            F,gF,G,JG = prob.eval_FG(x,w)
-
+            F,gF,G,JG = prob.eval_FG(x,w,debug=False)
+            
             # Lagrangian subgradient
             gL = gF + JG.T*lam
+
+            # Running
+            EF_run += 0.05*(F-EF_run)
+            EG_run += 0.05*(G-EG_run)
             
             # Show progress
             if not quiet:
@@ -51,10 +59,12 @@ class PrimalDual_StochasticGradient(StochasticSolver):
                 print '{0:^10.2f}'.format(t1-t0),
                 print '{0:^10.2f}'.format(prob.get_prop_x(x)),
                 print '{0:^10.2f}'.format(np.max(lam)),
-                if k % period == 0:
+                print '{0:^12.5e}'.format(EF_run),
+                print '{0:^12.5e}'.format(np.max(EG_run)),
+                if k % period == 0 and False:
                     EF,EgF,EG,EJG = prob.eval_EFG(x,samples=samples)
                     print '{0:^12.5e}'.format(EF),
-                    print '{0:^12.5e}'.format(np.max(EG)),
+                    print '{0:^12.5e}'.format(np.max(EG))
                     t0 += time.time()-t1
                 else:
                     print ''
