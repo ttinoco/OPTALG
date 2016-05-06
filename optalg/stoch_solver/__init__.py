035a264a6bef50b2077fa026319f2d513a081194
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2016-05-06 13:01:28 +0200

    update multi stage prob template

--- optalg/stoch_solver/__init__.py
+++ optalg/stoch_solver/__init__.py
@@ -1,10 +1,11 @@
 #****************************************************#
 # This file is part of OPTALG.                       #
 #                                                    #
-# Copyright (c) 2015, Tomas Tinoco De Rubira.        #
+# Copyright (c) 2015-2016, Tomas Tinoco De Rubira.   #
 #                                                    #
 # OPTALG is released under the BSD 2-clause license. #
 #****************************************************#
 
-from stoch_obj_prob import StochObj_Problem
 from stoch_gen_prob import StochGen_Problem
+from stoch_obj_prob import StochObj_Problem
+from stoch_obj_ms_prob import StochObjMS_Problem
