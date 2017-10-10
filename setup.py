4f0bc17d1381445deedf0558ecf0e16b95c6c761
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2017-10-10 10:30:28 -0700

    added manifest, updated setup.py (package description) and added script for uploading to pypi test

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ if os.environ.get('OPTALG_CLP') == 'true':
 setup(name='OPTALG',
       zip_safe=False,
       version='1.1.4rc1',
-      description='Optimization Algorithms',
+      description='Optimization Algorithms and Wrappers',
       author='Tomas Tinoco De Rubira',
       author_email='ttinoco5687@gmail.com',
       include_package_data=True,
