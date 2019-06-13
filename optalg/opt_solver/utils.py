dd7440b7194efad477f84933a6f63dc059cde3a8
Tomas Tinoco De Rubira <ttinoco5687@gmail.com>
2019-06-12 17:24:31 -0700

    os access x_ok for win

--- optalg/opt_solver/utils.py
+++ optalg/opt_solver/utils.py
@@ -1,6 +1,9 @@
 import os
+import sys
 import numpy as np
 
 def cmd_exists(cmd):
+    if 'win32' in sys.platform.lower():
+        cmd += '.exe'
     return any(os.access(os.path.join(path, cmd), os.X_OK)
                for path in os.environ["PATH"].split(os.pathsep))
